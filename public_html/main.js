b512c0c623d9ecdb9f349380a14db88d558ad435
solavrov <2slavrov@gmail.com>
2023-06-06 00:33:06 +0300

    storage

--- public_html/main.js
+++ public_html/main.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6dVE4Ic7AeH1uzeIHgDB_rrgo8EXrYu0",
+  authDomain: "diktant5.firebaseapp.com",
+  projectId: "diktant5",
+  storageBucket: "diktant5.appspot.com",
+  messagingSenderId: "702825342319",
+  appId: "1:702825342319:web:fbbc869aba354ce2a1349a",
+  measurementId: "G-ZP4BM4CFHD"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const storageRef = ref(storage);
 
 const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
@@ -6,9 +23,17 @@ const progress = document.getElementById("progress");
 const myAudio = document.createElement("audio");
 
 
+async function queryURL(fileName) {
+    const url = await getDownloadURL(ref(storage, fileName));
+    return url;
+}
+
+let currentURL = await queryURL("d2.mp3");
+
+
 playButton.addEventListener("click", function() {
     if (!myAudio.hasAttribute("src")) {
-        myAudio.setAttribute("src", "1.mp3");
+        myAudio.setAttribute("src", currentURL);
         myAudio.currentTime = 0;
     }
     myAudio.play();
